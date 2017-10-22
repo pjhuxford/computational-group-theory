52d02f18ea976e015eb7a43fbe3456726df0615d
Peter Huxford <pjhuxford@gmail.com>
2017-10-22 20:47:49 +1300

    Fix bug with recursion, and enforce divisibility condition

--- magma/SmithNormalForm.m
+++ magma/SmithNormalForm.m
@@ -1,33 +1,31 @@
 procedure Diagonalise(~A)
     m := NumberOfRows(A); n := NumberOfColumns(A);
     if not IsZero(A) and m ne 0 and n ne 0 then
+	// find a nonzero entry
 	i := 1; j := 1;
 	while A[i,j] eq 0 do
 	    if j lt n then
 		j +:= 1;
 	    else
-		i +:= 1;
-		j := 1;
+		i +:= 1; j := 1;
 	    end if;
 	end while;
 
+	// ensure A[i,j] divides everything in its row and column
 	r := 1; s := 1;
-	while r le m and s le n do
+	while r le m or s le n do
 	    if r le m then
 		if A[r,j] mod A[i,j] ne 0 then
 		    q := A[r,j] div A[i,j];
 		    AddRow(~A,-q,i,r);
-		    i := r;
-		    r := 1;
+		    i := r; r := 1;
 		else
 		    r +:= 1;
 		end if;
 	    elif A[i,s] mod A[i,j] ne 0 then
 		q := A[i,s] div A[i,j];
 		AddColumn(~A,-q,j,s);
-		j := s;
-		r := 1;
-		s := 1;
+		j := s; r := 1; s := 1;
 	    else
 		s +:= 1;
 	    end if;
@@ -36,6 +34,7 @@ procedure Diagonalise(~A)
 	SwapRows(~A,i,1);
 	SwapColumns(~A,j,1);
 
+	// clear out entries below and to the right of A[1,1]
 	for i in [2..m] do
 	    q := A[i,1] div A[1,1];
 	    AddRow(~A,-q,1,i);
@@ -53,4 +52,29 @@ procedure Diagonalise(~A)
 	Diagonalise(~C);
 	InsertBlock(~A,C,2,2);
     end if;
+end procedure;
+
+procedure SmithNormalForm(~A)
+    m := NumberOfRows(A); n := NumberOfColumns(A);
+    Diagonalise(~A);
+    r := 1;
+    while r le Min(m,n) do
+	if A[r,r] eq 0 then
+	    break;
+	else
+	    r +:= 1;
+	end if;
+    end while;
+    r -:= 1;
+
+    // enforce divisibility condition
+    for i in [1..r] do
+	for j in [i+1..r] do
+	    if A[j,j] mod A[i,i] ne 0 then
+		d := Gcd(A[i,i],A[j,j]);
+		l := A[i,i]*A[j,j] div d;
+		A[i,i] := d; A[j,j] := l;
+	    end if;
+	end for;
+    end for;
 end procedure;

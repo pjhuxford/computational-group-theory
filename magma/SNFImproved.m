ee59b98d3641fe6d676af9215acbced88a491968
Peter Huxford <pjhuxford@gmail.com>
2017-10-27 01:36:22 +1300

    Make small efficiency and readability improvements

--- magma/SNFImproved.m
+++ magma/SNFImproved.m
@@ -23,16 +23,16 @@ function hadamard(A)
 	    max := Max(max,sum);
 	end for;
 
-	return Min(Sqrt(prod), Sqrt(max^m));
+	return Sqrt(Min(prod, max^m));
     end if;
 end function;
 
 procedure DiagonaliseMod(~A,d)
-    Z := IntegerRing();
-    Z_d := IntegerRing(d);
-    A := ChangeRing(ChangeRing(A,Z_d),Z);
+    Z := IntegerRing(); Zd := IntegerRing(d);
     m := NumberOfRows(A); n := NumberOfColumns(A);
+
     if not IsZero(A) and m ne 0 and n ne 0 then
+	A := ChangeRing(A, Zd);
 	// find a nonzero entry
 	i := 1; j := 1;
 	while A[i,j] eq 0 do
@@ -47,20 +47,16 @@ procedure DiagonaliseMod(~A,d)
 	r := 1; s := 1;
 	while r le m or s le n do
 	    if r le m then
-		if A[r,j] mod A[i,j] ne 0 then
-		    q := A[r,j] div A[i,j];
-		    A := ChangeRing(A,Z_d);
+		if (Z ! A[r,j]) mod (Z ! A[i,j]) ne 0 then
+		    q := (Z ! A[r,j]) div (Z ! A[i,j]);
 		    AddRow(~A,-q,i,r);
-		    A := ChangeRing(A,Z);
 		    i := r; r := 1;
 		else
 		    r +:= 1;
 		end if;
-	    elif A[i,s] mod A[i,j] ne 0 then
-		q := A[i,s] div A[i,j];
-		A := ChangeRing(A,Z_d);
+	    elif (Z ! A[i,s]) mod (Z ! A[i,j]) ne 0 then
+		q := (Z ! A[i,s]) div (Z ! A[i,j]);
 		AddColumn(~A,-q,j,s);
-		A := ChangeRing(A,Z);
 		j := s; r := 1; s := 1;
 	    else
 		s +:= 1;
@@ -72,21 +68,15 @@ procedure DiagonaliseMod(~A,d)
 
 	// clear out entries below and to the right of A[1,1]
 	for i in [2..m] do
-	    q := A[i,1] div A[1,1];
-	    A := ChangeRing(A,Z_d);
+	    q := (Z ! A[i,1]) div (Z ! A[1,1]);
 	    AddRow(~A,-q,1,i);
-	    A := ChangeRing(A,Z);
 	end for;
 	for j in [2..n] do
-	    q := A[1,j] div A[1,1];
-	    A := ChangeRing(A,Z_d);
+	    q := (Z ! A[1,j]) div (Z ! A[1,1]);
 	    AddColumn(~A,-q,1,j);
-	    A := ChangeRing(A,Z);
 	end for;
 
-	if A[1,1] lt 0 then
-	    A[1,1] := -A[1,1];
-	end if;
+	A := ChangeRing(A, Z);
 
 	C := Submatrix(A, 2, 2, m-1, n-1);
 	DiagonaliseMod(~C,d);
@@ -112,23 +102,24 @@ procedure SmithNormalFormImproved(~A : primestart := 1000, dnumber := 3)
 	    prod *:= primes[#primes];
 	end while;
 
-	Submatrices := {};
+	submatrices := {};
 	rank := 0;
 
 	for p in primes do
+	    // row reduction modulo p
 	    // find the p-rank r of A
-	    // and an rxr submatrix with nonzero determinant (unless we have enough)
-	    RowIndices := [1..m];
-	    ColumnIndices := [];
+	    // and an rxr submatrix with full rank (unless we have enough)
+	    rows := [1..m];
+	    columns := [];
 
-	    Z_p := IntegerRing(p);
-	    A_p := ChangeRing(A, Z_p);
+	    Zp := IntegerRing(p);
+	    Ap := ChangeRing(A, Zp);
 
 	    i := 1; j := 1;
 	    while i le m and j le n do
 		k := i;
 		while k le m do
-		    if A_p[k,j] eq 0 then
+		    if Ap[k,j] eq 0 then
 			k +:= 1;
 		    else
 			break;
@@ -136,16 +127,16 @@ procedure SmithNormalFormImproved(~A : primestart := 1000, dnumber := 3)
 		end while;
 
 		if k le m then
-		    Append(~ColumnIndices, j);
+		    Append(~columns, j);
 		    if k ne i then
-			SwapRows(~A_p, i, k);
-			tmp := RowIndices[k];
-			RowIndices[k] := RowIndices[i];
-			RowIndices[i] := tmp;
+			SwapRows(~Ap, i, k);
+			tmp := rows[k];
+			rows[k] := rows[i];
+			rows[i] := tmp;
 		    end if;
-		    c := -1/A_p[i,j];
+		    c := -1/Ap[i,j];
 		    for l in [i+1..m] do
-			AddRow(~A_p, A_p[l,j]*c, i, l);
+			AddRow(~Ap, Ap[l,j]*c, i, l);
 		    end for;
 		    i +:= 1;
 		end if;
@@ -153,26 +144,27 @@ procedure SmithNormalFormImproved(~A : primestart := 1000, dnumber := 3)
 	    end while;
 
 	    r := i - 1;
-	    RowIndices := RowIndices[[1..r]];
-	    Sort(~RowIndices);
+	    rows := rows[[1..r]];
+	    Sort(~rows);
 	    if r gt rank then
 		rank := r;
-		Submatrices := {[RowIndices, ColumnIndices]};
-	    elif r eq rank and #Submatrices lt dnumber then
-		Include(~Submatrices, [RowIndices, ColumnIndices]);
+		submatrices := {[rows, columns]};
+	    elif r eq rank and #submatrices lt dnumber then
+		Include(~submatrices, [rows, columns]);
 	    end if;
 	end for;
 
-	// compute determinants of Submatrices
+	// compute determinants of submatrices
 	dets := [];
-	for indices in Submatrices do
-	    dets_p := [];
+	for indices in submatrices do
+	    rows := indices[1]; columns := indices[2];
+	    detsprimes := [];
 	    for p in primes do
-		Z_p := IntegerRing(p);
-		A_p := ChangeRing(A, Z_p);
-		Append(~dets_p, IntegerRing() ! Determinant(Submatrix(A, indices[1], indices[2])));
+		Z := IntegerRing(); Zp := IntegerRing(p);
+		Ap := ChangeRing(A, Zp);
+		Append(~detsprimes, Z ! Determinant(Submatrix(Ap, rows, columns)));
 	    end for;
-	    sol := CRT(dets_p, primes);
+	    sol := CRT(detsprimes, primes);
 	    altsol := sol - prod;
 	    if Abs(altsol) lt sol then
 		Append(~dets, altsol);
@@ -185,18 +177,18 @@ procedure SmithNormalFormImproved(~A : primestart := 1000, dnumber := 3)
 
 	// Compute the SNF of A modulo d
 	DiagonaliseMod(~A,d);
-	t := 1;
-	while t le Min(m,n) do
-	    if A[t,t] eq 0 then
+	s := 1;
+	while s le Min(m,n) do
+	    if A[s,s] eq 0 then
 		break;
 	    else
-		t +:= 1;
+		s +:= 1;
 	    end if;
 	end while;
-	t -:= 1;
+	s -:= 1;
 	// enforce divisibility condition
-	for i in [1..t] do
-	    for j in [i+1..t] do
+	for i in [1..s] do
+	    for j in [i+1..s] do
 		if A[j,j] mod A[i,i] ne 0 then
 		    g := Gcd(A[i,i],A[j,j]);
 		    l := A[i,i]*A[j,j] div g;
@@ -206,7 +198,7 @@ procedure SmithNormalFormImproved(~A : primestart := 1000, dnumber := 3)
 	end for;
 
 	// Recover SNF of A
-	for i in [t+1..rank] do
+	for i in [s+1..rank] do
 	    A[i,i] := d;
 	end for;
     end if;

f3cf01afd998e8c75d06dc7768b424cf8cd972af
Peter Huxford <pjhuxford@gmail.com>
2017-10-29 17:00:16 +1300

    Add maxtime parameter to SmithNormalForm procedure

--- magma/SmithNormalForm.m
+++ magma/SmithNormalForm.m
@@ -1,4 +1,4 @@
-procedure Diagonalise(~A, ~max)
+procedure Diagonalise(~A, ~max, ~overtime, maxtime)
     m := NumberOfRows(A); n := NumberOfColumns(A);
     if not IsZero(A) and m ne 0 and n ne 0 then
 	// find min nonzero modulus entry A[i,j]
@@ -31,91 +31,102 @@ procedure Diagonalise(~A, ~max)
 	// ensure A[i,j] divides everything in its row and column
 	r := 1; s := 1;
 	while r le m or s le n do
-	    if r le m then
-		if A[r,j] mod A[i,j] ne 0 then
-		    q := A[r,j] div A[i,j];
-		    AddRow(~A,-q,i,r);
-		    i := r; r := 1;
-		    for k in [1..n] do
-			if Abs(A[i,k]) gt max then
-			    max := Abs(A[i,k]);
+	    if Cputime() gt maxtime then
+		overtime := true;
+		print "overtime";
+		break;
+	    else
+		if r le m then
+		    if A[r,j] mod A[i,j] ne 0 then
+			q := A[r,j] div A[i,j];
+			AddRow(~A,-q,i,r);
+			i := r; r := 1;
+			for k in [1..n] do
+			    if Abs(A[i,k]) gt max then
+				max := Abs(A[i,k]);
+			    end if;
+			end for;
+		    else
+			r +:= 1;
+		    end if;
+		elif A[i,s] mod A[i,j] ne 0 then
+		    q := A[i,s] div A[i,j];
+		    AddColumn(~A,-q,j,s);
+		    j := s; r := 1; s := 1;
+		    for k in [1..m] do
+			if Abs(A[k,j]) gt max then
+			    max := Abs(A[k,j]);
 			end if;
 		    end for;
 		else
-		    r +:= 1;
+		    s +:= 1;
 		end if;
-	    elif A[i,s] mod A[i,j] ne 0 then
-		q := A[i,s] div A[i,j];
-		AddColumn(~A,-q,j,s);
-		j := s; r := 1; s := 1;
+	    end if;
+	end while;
+
+	if not overtime then
+	    SwapRows(~A,i,1);
+	    SwapColumns(~A,j,1);
+
+	    // clear out entries below and to the right of A[1,1]
+	    for i in [2..m] do
+		q := A[i,1] div A[1,1];
+		AddRow(~A,-q,1,i);
+		for k in [1..n] do
+		    if Abs(A[i,k]) gt max then
+			max := Abs(A[i,k]);
+		    end if;
+		end for;
+	    end for;
+	    for j in [2..n] do
+		q := A[1,j] div A[1,1];
+		AddColumn(~A,-q,1,j);
 		for k in [1..m] do
 		    if Abs(A[k,j]) gt max then
 			max := Abs(A[k,j]);
 		    end if;
 		end for;
-	    else
-		s +:= 1;
+	    end for;
+
+	    if A[1,1] lt 0 then
+		A[1,1] := -A[1,1];
 	    end if;
-	end while;
 
-	SwapRows(~A,i,1);
-	SwapColumns(~A,j,1);
-
-	// clear out entries below and to the right of A[1,1]
-	for i in [2..m] do
-	    q := A[i,1] div A[1,1];
-	    AddRow(~A,-q,1,i);
-	    for k in [1..n] do
-		if Abs(A[i,k]) gt max then
-		    max := Abs(A[i,k]);
-		end if;
-	    end for;
-	end for;
-	for j in [2..n] do
-	    q := A[1,j] div A[1,1];
-	    AddColumn(~A,-q,1,j);
-	    for k in [1..m] do
-		if Abs(A[k,j]) gt max then
-		    max := Abs(A[k,j]);
-		end if;
-	    end for;
-	end for;
-
-	if A[1,1] lt 0 then
-	    A[1,1] := -A[1,1];
-	end if;
-
-	C := Submatrix(A, 2, 2, m-1, n-1);
-	submax := 0;
-	Diagonalise(~C, ~submax);
-	InsertBlock(~A,C,2,2);
-	if submax gt max then
-	    max := submax;
+	    C := Submatrix(A, 2, 2, m-1, n-1);
+	    submax := 0;
+	    Diagonalise(~C, ~submax, ~overtime, maxtime);
+	    InsertBlock(~A,C,2,2);
+	    if submax gt max then
+		max := submax;
+	    end if;
 	end if;
     end if;
 end procedure;
 
-procedure SmithNormalForm(~A,~max)
+procedure SmithNormalForm(~A,~max : maxtime := 1800)
+    overtime := false;
     m := NumberOfRows(A); n := NumberOfColumns(A);
-    Diagonalise(~A,~max);
-    r := 1;
-    while r le Min(m,n) do
-	if A[r,r] eq 0 then
-	    break;
-	else
-	    r +:= 1;
-	end if;
-    end while;
-    r -:= 1;
-
-    // enforce divisibility condition
-    for i in [1..r] do
-	for j in [i+1..r] do
-	    if A[j,j] mod A[i,i] ne 0 then
-		d := Gcd(A[i,i],A[j,j]);
-		l := A[i,i]*A[j,j] div d;
-		A[i,i] := d; A[j,j] := l;
+    Diagonalise(~A,~max,~overtime,maxtime);
+    if not overtime then
+	r := 1;
+	while r le Min(m,n) do
+	    if A[r,r] eq 0 then
+		break;
+	    else
+		r +:= 1;
 	    end if;
+	end while;
+	r -:= 1;
+
+	// enforce divisibility condition
+	for i in [1..r] do
+	    for j in [i+1..r] do
+		if A[j,j] mod A[i,i] ne 0 then
+		    d := Gcd(A[i,i],A[j,j]);
+		    l := A[i,i]*A[j,j] div d;
+		    A[i,i] := d; A[j,j] := l;
+		end if;
+	    end for;
 	end for;
-    end for;
+    end if;
 end procedure;
